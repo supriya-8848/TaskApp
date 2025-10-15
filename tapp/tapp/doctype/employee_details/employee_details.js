af06a7e2c8720a9b0c20de80711dd5289198fbcf
supriya-8848 <supriya@8848digital.com>
2025-10-15 13:06:15 +0530

    fix:updated function to handle edge cases

---
+++ tapp/tapp/doctype/employee_details/employee_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, supriya and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Employee Details", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ tapp/tapp/doctype/employee_details/test_employee_details.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, supriya and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEmployeeDetails(FrappeTestCase):
+	pass
